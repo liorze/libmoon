0ad36b535450f1fa6edfff887d8b148fa53ffd6f
Lior Zeno <liorz@tx.technion.ac.il>
2021-03-22 13:56:02 +0000

    libmoon with dpdk v20.08

--- build.sh
+++ build.sh
@@ -58,7 +58,7 @@ make install DESTDIR=$(pwd)
 (
 cd deps/dpdk
 #build DPDK with the right configuration
-make config T=x86_64-native-linux-gcc O=x86_64-native-linux-gcc
+make config T=x86_64-native-linux-gcc RTE_OUTPUT=x86_64-native-linux-gcc
 sed -ri 's,(CONFIG_RTE_LIBRTE_IEEE1588=).*,\1y,' x86_64-native-linux-gcc/.config
 if ${MLX5} ; then
 	sed -ri 's,(MLX5_PMD=).*,\1y,' x86_64-native-linux-gcc/.config
